dabd3e083a59625ef6e0ea54fc9938f746a2cb29
Badlose <git@github.com:Badlose/helloworld.git>
2025-03-19 22:26:36 +0300

    Project structure. Controller, Service, Impl

---
+++ bank/src/main/java/ru/starbank/bank/Service/RecommendationService.java
@@ -0,0 +1,11 @@
+package ru.starbank.bank.Service;
+
+import ru.starbank.bank.Model.Recommendation;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RecommendationService {
+    Optional<Recommendation> getRecommendation(UUID userId);
+
+}
