09f36e958d0afa1fd9fed7c141937a537332d287
UPUHAK <89092936911@mail.ru>
2025-03-22 14:39:36 +0300

    RuleSet checking1

--- bank/src/main/java/ru/starbank/bank/Service/Impl/RecommendationServiceImpl.java
+++ bank/src/main/java/ru/starbank/bank/Service/Impl/RecommendationServiceImpl.java
@@ -1,31 +1,23 @@
 package ru.starbank.bank.Service.Impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.starbank.bank.Model.Recommendation;
-import ru.starbank.bank.Service.Impl.RecommendationRuleSetImpl.RecommendationRuleSetOneImpl;
 import ru.starbank.bank.Service.RecommendationRuleSet;
 import ru.starbank.bank.Service.RecommendationService;
-import ru.starbank.bank.Service.Impl.RecommendationRuleSetImpl.RecommendationRuleSetTwoImpl;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
 public class RecommendationServiceImpl implements RecommendationService {
 
-    private RecommendationRuleSet ruleSet;
-
-    public RecommendationServiceImpl(RecommendationRuleSet ruleSet) {
-        this.ruleSet = ruleSet;
-    }
-
-//    RecommendationRuleSet ruleSet2;
-//    RecommendationRuleSet ruleSet3;
 
     @Override
-    public Optional<Recommendation> getRecommendation(UUID userId) {
+    public Optional<List<Recommendation>> getRecommendation(UUID userId) {
 
-        ruleSet.check(userId); //бины
+        //List<Recommendation> //бины
 
         return Optional.empty();
     }
--- bank/src/main/java/ru/starbank/bank/Service/RecommendationService.java
+++ bank/src/main/java/ru/starbank/bank/Service/RecommendationService.java
@@ -2,9 +2,10 @@ package ru.starbank.bank.Service;
 
 import ru.starbank.bank.Model.Recommendation;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RecommendationService {
-    Optional<Recommendation> getRecommendation(UUID userId);
+    Optional<List<Recommendation>> getRecommendation(UUID userId);
 }
