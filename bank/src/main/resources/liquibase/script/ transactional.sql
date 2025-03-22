71fb2b75b2f30693e9150b82728f43623b26a184
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2025-03-22 11:06:37 +0300

    combining databases_1

--- bank/src/main/resources/liquibase/script/ transactional.sql
+++ bank/src/main/resources/liquibase/script/ transactional.sql
@@ -1,3 +1,15 @@
-databaseChangeLog:
-  - include:
-      file: liquibase/scripts/transactional.sql
+changelog-master.yaml
+-- changeset dmitriy:1
+
+SELECT *
+FROM (
+    SELECT t.ID AS TRANSACTION_ID, 
+           t.USER_ID, 
+           t.type AS transaction_type, 
+           t.AMOUNT, 
+           p.ID AS PRODUCT_ID, 
+           p.TYPE AS PRODUCT_TYPE, 
+           p.NAME AS PRODUCT_NAME
+    FROM TRANSACTIONS t
+    JOIN PRODUCTS p ON t.product_id = p.id
+) AS newTable;
