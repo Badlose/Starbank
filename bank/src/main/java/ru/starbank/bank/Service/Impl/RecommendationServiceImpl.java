becc0dc10d3fcd2f8df4f2c185e1ce7f8c3dd7bc
Badlose <git@github.com:Badlose/helloworld.git>
2025-03-24 02:32:16 +0300

    Completed RuleSets

--- bank/src/main/java/ru/starbank/bank/Service/Impl/RecommendationServiceImpl.java
+++ bank/src/main/java/ru/starbank/bank/Service/Impl/RecommendationServiceImpl.java
@@ -1,47 +1,33 @@
 package ru.starbank.bank.Service.Impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.starbank.bank.Controller.RecommendationController;
 import ru.starbank.bank.Model.Recommendation;
 import ru.starbank.bank.Service.RecommendationRuleSet;
 import ru.starbank.bank.Service.RecommendationService;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
 public class RecommendationServiceImpl implements RecommendationService {
-    private static final Logger logger = LoggerFactory.getLogger(RecommendationController.class);
 
-    @Override
-    public Optional<List<Recommendation>> getRecommendation(UUID userId) {
+    @Autowired
+    private final List<RecommendationRuleSet> ruleSets;
 
-        //List<Recommendation> //бины
-
-        return Optional.empty();
-    }
-
-
-    public List<Recommendation> getRecommendationsWithLogging(UUID userId) {
-        logger.info("Запрос на получение рекомендаций для пользователя с ID: {}", userId);
-        List<Recommendation> recommendations = getRecommendation(userId).orElseGet(Collections::emptyList);
-        if (!recommendations.isEmpty()) {
-            logger.info("Рекомендации найдены для пользователя с ID: {}", userId);
-        } else {
-            logger.info("Рекомендации не найдены для пользователя с ID: {}", userId);
-        }
-        return recommendations;
+    public RecommendationServiceImpl(List<RecommendationRuleSet> ruleSets) {
+        this.ruleSets = ruleSets;
     }
 
     @Override
-    public Integer getAmount(UUID userId) {
+    public List<Recommendation> getRecommendation(UUID userId) {
 
-        return 0;
+        return ruleSets.stream()
+                .map(r -> r.check(userId))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .toList();
     }
 
 }
