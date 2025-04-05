ed7ecf9d8e39e826fa2eba8b43cc01ca9ebec5bf
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-04-05 06:27:51 +0300

    UPDATE exception for ResponseStatusException(HttpStatus.BAD_REQUEST

--- bank/src/main/java/ru/starbank/bank/service/Impl/CheckCorrect.java
+++ bank/src/main/java/ru/starbank/bank/service/Impl/CheckCorrect.java
@@ -1,7 +1,9 @@
 package ru.starbank.bank.service.Impl;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import ru.starbank.bank.model.DynamicRecommendation;
 import ru.starbank.bank.model.Rule;
 import ru.starbank.bank.model.enums.QueryEnum;
@@ -14,15 +16,15 @@ public class CheckCorrect {
     public boolean checkRecommendationCorrect(DynamicRecommendation recommendation){
         List<String> comparisons = List.of("<",">","<=",">=","==");
         if(recommendation == null){
-            throw new RuntimeException("recommendation == null");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid field");
         }if (recommendation.getName()==null || recommendation.getProductId()==null || recommendation.getText()==null || recommendation.getRuleList()==null) {
-            throw new RuntimeException("invalid field");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid field");
         } else {
             for(Rule rule: recommendation.getRuleList()){
                 if(rule.getArguments()==null || rule.getQuery()==null){
-                    throw new RuntimeException("invalid field");
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid field");
                 }if(rule.getArguments().isEmpty()){
-                    throw new RuntimeException("invalid field");
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid field");
                 }
                 boolean errorType = false;
                 try {
@@ -32,16 +34,16 @@ public class CheckCorrect {
                     errorType = true;
                 }
                 if(errorType){
-                    throw new RuntimeException("invalid type");
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid type");
                 }
                 if(rule.getArguments().size()==2 && (!comparisons.contains(rule.getArguments().get(1)) || rule.getArguments().get(1).length()>2)) {
-                    throw new RuntimeException("invalid comparison");
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid comparison");
                 }
                 if(rule.getArguments().size()==4 && ((!rule.getArguments().get(1).equals("WITHDRAW") && !rule.getArguments().get(1).equals("DEPOSIT")) || (!comparisons.contains(rule.getArguments().get(2)) || rule.getArguments().get(2).length()>2) || !StringUtils.isNumeric(rule.getArguments().get(3)))){
-                    throw new RuntimeException("invalid comparison or type transaction or number");
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid comparison or type transaction or number");
                 }
                 if(rule.getArguments().size()==5 && ((!rule.getArguments().get(1).equals("WITHDRAW") && !rule.getArguments().get(1).equals("DEPOSIT")) || (!comparisons.contains(rule.getArguments().get(2)) || rule.getArguments().get(2).length()>2) || !StringUtils.isNumeric(rule.getArguments().get(3)) || !rule.getArguments().get(3).equals("OR"))){
-                    throw new RuntimeException("invalid comparison or type transaction or number or OR");
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"invalid comparison or type transaction or number or OR");
                 }
             }
         }
