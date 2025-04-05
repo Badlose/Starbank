2952c6e2498842d8d8b40732880d360fbeddfe41
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2025-04-05 17:14:11 +0300

    telegramBot_3

--- bank/src/main/java/ru/starbank/bank/telegram/service/MessageProcessor.java
+++ bank/src/main/java/ru/starbank/bank/telegram/service/MessageProcessor.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.starbank.bank.repository.RecommendationsRepository;
 import ru.starbank.bank.service.RecommendationService;
+import ru.starbank.bank.telegram.service.impl.MessageServiceImpl;
+
+import java.util.UUID;
 
 
 @Component
@@ -18,18 +21,24 @@ public class MessageProcessor {
 
     private final MessageSender messageSender;
     private final RecommendationService recommendationService;
+    private final MessageServiceImpl messageService;
+
 
 
     @Autowired
     private TelegramBot telegramBot;
 
-    public MessageProcessor(RecommendationsRepository recommendationsRepository, MessageSender messageSender, RecommendationService recommendationService) {
+    public MessageProcessor(RecommendationsRepository recommendationsRepository, MessageSender messageSender, RecommendationService recommendationService, MessageProcessor messageProcessor, MessageServiceImpl messageService) {
         this.recommendationService = recommendationService;
         this.messageSender = messageSender;
+
+        this.messageService = messageService;
     }
 
 
     public void processMessage(String messageText, long chatId) {
+
+
 
 
 
