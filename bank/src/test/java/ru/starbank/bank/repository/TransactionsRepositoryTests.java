95d4ee416f9de31e9e6e7c57e5c34bcc28d8a94c
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-04-10 14:24:32 +0300

    clearCacheTest is not working

--- bank/src/test/java/ru/starbank/bank/repository/TransactionsRepositoryTests.java
+++ bank/src/test/java/ru/starbank/bank/repository/TransactionsRepositoryTests.java
@@ -2,27 +2,47 @@ package ru.starbank.bank.repository;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.TestPropertySource;
+import ru.starbank.bank.configuration.CacheConfigurations;
 import ru.starbank.bank.configuration.RecommendationsDataSourceConfiguration;
+import ru.starbank.bank.service.Impl.ManagementServiceImpl;
 
 import java.util.UUID;
 
-
+@SpringBootTest
 //@TestPropertySource("/application.properties")
 @Import(value = RecommendationsDataSourceConfiguration.class)
 public class TransactionsRepositoryTests {
-
+    private static final Logger logger = LoggerFactory.getLogger(TransactionsRepositoryTests.class);
     private final TransactionsRepository repository;
-    private final RecommendationsDataSourceConfiguration configuration;
+    @Autowired
+    private final CacheManager cacheManager;
+    private final CacheConfigurations cacheConfigurations;
+    private final RecommendationsDataSourceConfiguration dataSourceConfiguration;
     public TransactionsRepositoryTests() {
-        this.configuration = new RecommendationsDataSourceConfiguration();
-        this.repository = new TransactionsRepository(configuration.recommendationsJdbcTemplate(configuration.recommendationsDataSource("jdbc:h2:file:./transactionTests")));
+        this.dataSourceConfiguration = new RecommendationsDataSourceConfiguration();
+        this.repository = new TransactionsRepository(dataSourceConfiguration.recommendationsJdbcTemplate(dataSourceConfiguration.recommendationsDataSource("jdbc:h2:file:./transactionTests")));
+        this.cacheConfigurations= new CacheConfigurations();
+        this.cacheManager= cacheConfigurations.cacheManager();
     }
+
+    public boolean cacheIsEmpty(String cacheName){
+        Cache cache = cacheManager.getCache(cacheName);
+        Object nativeCache = cache.getNativeCache();
+        if (nativeCache instanceof com.github.benmanes.caffeine.cache.Cache) {
+            com.github.benmanes.caffeine.cache.Cache caffeineCache = (com.github.benmanes.caffeine.cache.Cache) nativeCache;
+            return caffeineCache.estimatedSize() == 0;
+        } else {
+            return false;
+        }
+    }
+
 
     @Test
     public void shouldReturnCountTransactionsByUserIdProductType() {
@@ -77,5 +97,23 @@ public class TransactionsRepositoryTests {
         UUID actualId = repository.getUserIdByUserName(UserName);
 
         Assertions.assertEquals(expectedId,actualId);
+    }
+
+    @Test
+    public void shouldReturnClearAllCaches(){
+        UUID userId = UUID.fromString("cd515076-5d8a-44be-930e-8d4fcb79f42d");
+        String productType = "DEBIT";
+        String cacheName = "transactionCounts";
+
+        Assertions.assertTrue(cacheIsEmpty(cacheName));
+        logger.info(cacheManager.getCacheNames().toString());
+        repository.countTransactionsByUserIdProductType(userId,productType);
+        logger.info(String.valueOf(repository.countTransactionsByUserIdProductType(userId,productType)));
+        //logger.info(cacheManager.);
+        Assertions.assertFalse(cacheIsEmpty(cacheName));
+
+        repository.clearCache();
+
+        Assertions.assertTrue(cacheIsEmpty(cacheName));
     }
 }
